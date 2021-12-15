2bfd0f78f2a1e562209aea0e7c7eb27997cce52d
Ivan <jerzabek.ivan@gmail.com>
2021-12-15 16:44:28 +0100

    Obrisan originalni constants.php

--- constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Database access configuration
-define('DB_NAME', 'fer_2122_or_mobilni_uredaji');
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
