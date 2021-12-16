cceea3f9a454113019e2dd815dfe7245bb056ad8
Ivan <jerzabek.ivan@gmail.com>
2021-12-16 15:53:29 +0100

    #11, #12 Spajanje na DB i /api/phones/all ruta

--- api/or_api.py
+++ api/or_api.py
@@ -1,26 +1,46 @@
+import sys
+
 from flask import Flask
 from flask_cors import CORS
-import or_api_utils as util
 from http import HTTPStatus
 
 import or_api_config as cfg
+import or_api_utils as util
+from or_api_db import OR_API_DB
 
+# Loading configuration, used to store constant variables and environment variables
 cfg.load_config()
 
 debug_mode = cfg.get_config("debug_mode")
 ssl_enabled = cfg.get_config("ssl_enabled")
 
+# Database configuration object
+db_config = {
+    'user':                 cfg.get_config("db_username"),
+    'password':             cfg.get_config("db_password"),
+    'host':                 cfg.get_config("db_server"),
+    'database':             cfg.get_config("db_name"),
+    'raise_on_warnings':    True
+}
+
+db = OR_API_DB(db_config)
+
+# If we were unable to connect to the database we will exit the application
+if(not db.connect()):
+    sys.exit()
+
 app = Flask(__name__)
 CORS(app)
 
-
-def connect_to_db():
-    pass
-
-
-@app.route('/hello', methods=['GET'])
+@app.route('/phone/all', methods=['GET'])
 def hello():
-    return util.build_response({"success": True, "response": {"message": "Hello world!!"}}), HTTPStatus.OK
+    cursor = db.get_cursor()
+
+    cursor.execute("SELECT * FROM phone;")
+    data = cursor.fetchall()
+    cursor.close()
+
+    return util.build_response({"success": True, "response": data}), HTTPStatus.OK
 
 
 # Catch all route - used to match routes that do not exist
@@ -31,14 +51,18 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
+    # Flask webhost configuration
     flask_config = {
         "host": cfg.get_config("flask_host"),
         "port": cfg.get_config("flask_port"),
         "debug": debug_mode,
     }
 
+    # HTTPS
     if ssl_enabled:
-        flask_config["ssl_context"] = (cfg.get_config(
-            "certfile"), cfg.get_config("keyfile"))
+        flask_config["ssl_context"] = (
+            cfg.get_config("certfile"),
+            cfg.get_config("keyfile")
+        )
 
     app.run(**flask_config)
